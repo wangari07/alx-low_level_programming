51d8f956caf3433e2ad184ab115f5122cc854e8f
wangari07 <cwangari076@gmail.com>
2023-02-16 16:54:19 -0800

    C - Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**[
+ * main - A program that prints a line with puts function
+ */
+int main(void)
+{
+puts("/"Programming is like building a multilingual puzzle");
+return (0)
+}	
